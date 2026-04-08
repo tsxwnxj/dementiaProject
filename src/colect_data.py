fcb0debf42bce2044f2394ebdb578df4636a7ae5
jaemin8786-lab <jaemin8786@gmail.com>
2026-04-09 02:02:10 +0900

    데이터추가

--- src/colect_data.py
+++ src/colect_data.py
@@ -9,7 +9,7 @@ ALL_GESTURES = {
     "1": "finger_wave",     # 1. 손가락 움직이기
     "2": "hand_shake",      # 2. 손털기
     "3": "finger_fold",     # 3. 손가락 접기
-    "4": "fist_open",       # 4. 주먹 쥐고 펴기
+    "4": "fist_open",       # 4. 주먹 쥐고 펴기 
     "5": "cross_fist",      # 5. 엇갈려 주먹 쥐고 펴기
     "6": "fingertip_clap",  # 6. 손끝 박수
 }
